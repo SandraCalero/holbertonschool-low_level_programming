802cfc8c80dec8306486ac0468c7c12bb6f1a7aa
Sandra Calero <3358@holbertonschool.com>
2021-06-17 23:07:05 -0500

    add C program that prints a text and a special character

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Prints a text to in the standard error.
- * Return: 1 success.
+ * main - Prints a text to in the standard error
+ *
+ * Return: 1 success
  */
 int main(void)
 {
-char text;
-system("echo and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",text);
+system("echo and that piece of art is useful\" - Dora Korpar, 2015-10-1");
 return (1);
 }
