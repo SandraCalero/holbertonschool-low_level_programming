7a69626d97254eb13f45bd8889752b27c1e101ab
Sandra Calero <3358@holbertonschool.com>
2021-06-17 22:36:14 -0500

    add C program that prints a text and a special character

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints a text to in the standard error.
+ * Return: 1 success.
+ */
+int main(void)
+{
+char text;
+scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",text);
+return (1);
+}
