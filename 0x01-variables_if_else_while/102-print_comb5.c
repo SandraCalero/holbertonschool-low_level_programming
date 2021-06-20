e812269a4c8353000e036d9d59f1744a767e1f52
Sandra Calero <3358@holbertonschool.com>
2021-06-20 18:16:04 -0500

    add prints all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of two two-digit numbers
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+int num1, num2, num3, num4;
+for (num1 = 48; num1 <= 57; num1++)
+{
+for (num2 = 48; num2 <= 56; num2++)
+{
+for (num3 = 48; num3 <= 57; num3++)
+{
+for (num4 = 49; num4 <= 57; num4++)
+if (num1 >= num2 && num1 <= num3 && num2 <= num3 && num2 < num4)
+{
+putchar(num1);
+putchar(num2);
+putchar(' ');
+putchar(num3);
+putchar(num4);
+if ((num1 < 57 && num2 <= 56) || (num1 == 57 && num2 < 56))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
