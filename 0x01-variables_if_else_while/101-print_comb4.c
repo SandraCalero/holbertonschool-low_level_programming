7626b31b2ba06b5ba45429c0a1be85d4e14f95f8
Sandra Calero <3358@holbertonschool.com>
2021-06-20 16:18:20 -0500

    add program that prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 int num1, num2, num3;
-for (num1 = 48; num1 <= 55;num1++)
+for (num1 = 48; num1 <= 55; num1++)
 {
 for (num2 = 49; num2 <= 56; num2++)
 {
 for (num3 = 50; num3 <= 57; num3++)
-if(num1 < num2 && num2 < num3)
+if (num1 < num2 && num2 < num3)
 {
-putchar(num1); 
+putchar(num1);
 putchar(num2);
 putchar(num3);
 if (num1 < 55)
