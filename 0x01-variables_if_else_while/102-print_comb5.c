457cadbd6ba0e742acde976c31139ec4fa778eca
Sandra Calero <3358@holbertonschool.com>
2021-06-20 20:53:34 -0500

    add prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,7 +14,7 @@ for (num2 = 48; num2 <= 56; num2++)
 sum1 = num1 * 100 + num2;
 for (num3 = 48; num3 <= 57; num3++)
 {
-for (num4 = 49; num4 <= 57; num4++)
+for (num4 = 48; num4 <= 57; num4++)
 {
 sum2 = num3 * 100 + num4;
 if (sum1 < sum2)
