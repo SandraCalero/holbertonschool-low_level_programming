5a468219bbef7acd47a7716c0b14a02c73c26816
Sandra Calero <3358@holbertonschool.com>
2021-06-22 18:50:16 -0500

    add a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: No exist
+ */
+void jack_bauer(void)
+{
+	int num1, num2;
+		for (num1 = 0; num1 <= 23; num1++)
+		{
+			for (num2 = 0; num2 <= 59; num2++)
+			{
+
+				_putchar((num1 / 10) + '0');
+				_putchar((num1 % 10) + '0');
+				_putchar(':');
+				_putchar((num2 / 10) + '0');
+				_putchar((num2 % 10) + '0');
+				_putchar('\n');
+
+			}
+		}
+}
