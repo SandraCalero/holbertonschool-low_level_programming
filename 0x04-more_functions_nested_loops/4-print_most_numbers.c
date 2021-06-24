c1f62774c60450f78c0c5e8da79ed01f8355dc7e
Sandra Calero <3358@holbertonschool.com>
2021-06-24 13:10:23 -0500

    add a function that prints the numbers, from 0 to 9 but do not print 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - Prints the numbers, from 0 to 9, Do not print 2 and 4
+ *
+ * Return: No exist.
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+{
+	if (c != '2' && c != '4')
+	_putchar(c);
+}
+	_putchar('\n');
+}
