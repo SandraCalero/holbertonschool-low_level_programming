657350970d4e37137554ef113ce143353f79a723
Sandra Calero <3358@holbertonschool.com>
2021-06-24 21:51:27 -0500

    add The Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ * main - Prints the numbers from 1 to 100, FizzBuzz.
+ *
+ * Return: 0 Always
+ */
+int main()
+{
+	int num;
+	for (num = 1 ; num <= 100 ; num++)
+	{
+		if (num == 100)
+		{
+			printf("Buzz\n");
+		}
+		else if (num%3 == 0 && num%5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num%3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num%5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (num < 100)
+		{
+			printf("%d ", num);
+		}
+
+	}
+	return (0);
+}
