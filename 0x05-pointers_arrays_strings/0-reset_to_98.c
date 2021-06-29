4fda563595365a17d2e78598db2fd1f8a7a20a73
Sandra Calero <3358@holbertonschool.com>
2021-06-29 12:31:51 -0500

    add  a function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 -  updates the value of an integrer to 98
+ *
+ * @n: a pointer to an int we want to updates to 98
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
