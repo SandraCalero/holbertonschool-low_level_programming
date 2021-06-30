a366eea0bba56e5e068035a0b3f96236d2e4d417
Sandra Calero <3358@holbertonschool.com>
2021-06-29 20:36:16 -0500

    add a function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: a pointer to a string we want to print
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+	char c;
+	int size, i;
+
+	for (size = 0 ; size[s] != '\0' ; size++)
+		;
+	size = size - 1;
+	for (i = 0 ; i < (size / 2) ; i++)
+	{
+		c = s[i];
+		s[i] = s[size - i];
+		s[size - i] = c;
+	}
+}
