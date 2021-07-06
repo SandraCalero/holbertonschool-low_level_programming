477fe376c6f3d8a4b2aff1f782d5893016f57650
Sandra Calero <3358@holbertonschool.com>
2021-07-05 23:59:17 -0500

    add a function that prints the sum of the two diagonals of a square matrix of integers

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+
+/**
+ * _strstr - locates a substring.
+ *
+ * @haystack: a pointer to a string
+ * @needle: substring in the string haystack
+ * Return:  a pointer to the beginning of the located substring
+ */
