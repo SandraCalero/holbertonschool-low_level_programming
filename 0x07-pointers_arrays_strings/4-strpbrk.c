f45fc34766b99b062e8cb1469d2358a3d9addcd8
Sandra Calero <3358@holbertonschool.com>
2021-07-05 21:48:19 -0500

    add a function that searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ *
+ * @s: a pointer to a string
+ * @accept: a pointer to a string
+ * Return:  a pointer to the byte in s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+
+	while (*s != '\0')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+			if (*s == accept[i])
+				return (s);
+		s++;
+	}
+	return (s + 1);
+}
