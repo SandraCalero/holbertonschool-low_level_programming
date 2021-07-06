ab03e03408582317ab94fc5f50502e1e447a7fbf
Sandra Calero <3358@holbertonschool.com>
2021-07-05 21:54:58 -0500

    add a function that searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,8 +14,10 @@ char *_strpbrk(char *s, char *accept)
 	while (*s != '\0')
 	{
 		for (i = 0; accept[i] != '\0'; i++)
+		{
 			if (*s == accept[i])
 				return (s);
+		}
 		s++;
 	}
 	return (s + 1);
