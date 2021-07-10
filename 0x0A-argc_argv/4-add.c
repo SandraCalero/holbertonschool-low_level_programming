4b2ecd2820968f0578d4889eb76cccaae8dcde6c
Sandra Calero <3358@holbertonschool.com>
2021-07-09 23:28:19 -0500

    add a program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,15 +19,17 @@ int main(int argc, char *argv[])
 		for (i = 1 ; i < argc ; i++)
 		{
 			for (j = 0; argv[i][j]; j++)
+			{
 				if (argv[i][j] >= '0' && argv[i][j] <= '9')
 				{
-					sum += atoi(argv[i]);
 				}
 				else
 				{
 					printf("Error\n");
 					return (1);
 				}
+			}
+		sum += atoi(argv[i]);
 		}
 		printf("%d\n", sum);
 		return (0);
