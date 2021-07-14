70e877318349ba639b5f4b4dcd1b555d3a120e2e
Sandra Calero <3358@holbertonschool.com>
2021-07-14 13:55:05 -0500

    add a function that returns a pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	p = malloc(sizeof(int) * height);
+	p = (int **)malloc(sizeof(int *) * height);
 	if (p == NULL)
 	{
 		free(p);
