c1d9d24550045e741f4ca603bcada59fa911ffca
Sandra Calero <3358@holbertonschool.com>
2021-07-21 22:15:06 -0500

    add a program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations
+ *
+ * @argc: count of the arguments supplied to the program
+ * @argv: array of pointers to the strings.
+ * Return:  0 success.
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
+	    && *argv[2] != '/' && *argv[2] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (atoi(argv[3]) == 0 && *argv[2] != '/' && *argv[2] != '%')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = (get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - sums two integers
+ * @a: first number to sum
+ * @b: second number to sum
+ *
+ * Return: The result of a sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts two integers
+ * @a: first number to subtract
+ * @b: second number to subtract
+ *
+ * Return: The result of a subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two integers
+ * @a: first number to multiply
+ * @b: second number to multiply
+ *
+ * Return: The result of a multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two integers
+ * @a: first number to divide
+ * @b: second number to divide
+ *
+ * Return: The result of a division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - calculates the remainder of a division
+ * @a: dividend
+ * @b: divisor
+ *
+ * Return: The remainder of the division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
