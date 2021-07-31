a4c642773c20d5fab7ded0be21bc1f0ddba5a015
Sandra Calero <3358@holbertonschool.com>
2021-07-30 19:27:59 -0500

    add stdlib.h library in the header file and add a function that prints all the elements of a list_t list.

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list -  prints all the elements of a list_t list.
+ * @h: Pointer that points to an element of the list
+ * Return: The number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t numNodes;
+
+	for (numNodes = 0 ; h != NULL ; numNodes++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (numNodes);
+}
