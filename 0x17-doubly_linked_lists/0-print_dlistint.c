6a2a75edd288e1191b508f4018c03d8486227fa0
SandraCalero <3358@holbertonschool.com>
2021-09-16 15:59:07 -0500

    adda function that prints all the elements of a dlistint_t list.

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: Pointer to the head to a double linked list
+ *
+ * Return: The number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t num_nodes = 0;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		num_nodes++;
+	}
+	return (num_nodes);
+}
