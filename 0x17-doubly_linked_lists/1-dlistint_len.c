13ddd104c2366392560fcad0bf5ebc1587c75417
SandraCalero <3358@holbertonschool.com>
2021-09-16 16:19:51 -0500

    add a function that returns the number of elements in a linked dlistint_t list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * dlistint_len - gets the number of elements in a linked dlistint_t list
+ * @h: Pointer to the head to a double linked list
+ *
+ * Return: The number of elements in a linked dlistint_t list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t num_nodes = 0;
+	const dlistint_t *current = h;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		num_nodes++;
+	}
+	return (num_nodes);
+}
