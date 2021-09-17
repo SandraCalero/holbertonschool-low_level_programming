3cef07b3fce5f1a226a8fadd6e61559ccdb1104e
SandraCalero <3358@holbertonschool.com>
2021-09-17 00:18:03 -0500

    fix betty style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- * print_dlistint - prints all the elements of a dlistint_t list
- * @h: Pointer to the head to a double linked list
- *
- * Return: The number of nodes
+* print_dlistint - prints all the elements of a dlistint_t list
+* @h: Pointer to the head to a double linked list
+*
+* Return: The number of nodes
 */
 size_t print_dlistint(const dlistint_t *h)
 {
