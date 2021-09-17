1a536aa9946d6701d00cb0b41221d26be8a27326
SandraCalero <3358@holbertonschool.com>
2021-09-17 00:20:06 -0500

    fix betty style

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
- * dlistint_len - gets the number of elements in a linked dlistint_t list
- * @h: Pointer to the head to a double linked list
- *
- * Return: The number of elements in a linked dlistint_t list
- */
+* dlistint_len - gets the number of elements in a linked dlistint_t list
+* @h: Pointer to the head to a double linked list
+*
+* Return: The number of elements in a linked dlistint_t list
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t num_nodes = 0;
