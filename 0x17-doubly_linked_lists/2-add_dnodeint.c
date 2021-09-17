634fecfac3c3d4b72d682e7de0fd79ea8adb41ab
SandraCalero <3358@holbertonschool.com>
2021-09-17 00:21:36 -0500

    fix betty style

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 /**
- * add_dnodeint - adds a new node at the beginning of a dlistint_t list
- * @head: Pointer to the head to a double linked list
- * @n: Integer as a data for the new node
- *
- * Return: The address of the new element, or NULL if it failed
- */
+* add_dnodeint - adds a new node at the beginning of a dlistint_t list
+* @head: Pointer to the head to a double linked list
+* @n: Integer as a data for the new node
+*
+* Return: The address of the new element, or NULL if it failed
+*/
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
