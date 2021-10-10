a12c78eb022e8fc31fb491f7a53a57663c92dc29
SandraCalero <3358@holbertonschool.com>
2021-10-10 01:35:28 -0500

    add a function that deletes a hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -11,17 +11,15 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *current;
 
-	if (ht == NULL)
-		return;
 	for (i = 0 ; i < ht->size ; i++)
 	{
 		while (ht->array[i] != NULL)
 		{
-			current = ht->array[i];
-			ht->array[i] = ht->array[i]->next;
-			free(current->key);
-			free(current->value);
-			free(current);
+			current = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = current;
 		}
 
 	}
