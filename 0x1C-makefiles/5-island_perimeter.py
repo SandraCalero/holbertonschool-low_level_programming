6662aeb3785dd341d482063014c4032e985c7931
Sandra Calero <3358@holbertonschool.com>
2021-11-29 22:34:11 -0500

    add a function that returns the perimeter of the island described in grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,5 +15,20 @@ def island_perimeter(grid):
             (not diagonally).
             - Grid is rectangular, width and height don’t exceed 100
     """
-    perimeter = 12
+    perimeter = 0
+    row = len(grid)
+    column = len(grid[0])
+    for i in range(row):
+        for j in range(column):
+            if grid[i][j] == 0:
+                continue
+            perimeter += 4
+            if i > 0:
+                perimeter -= grid[i - 1][j]
+            if j > 0:
+                perimeter -= grid[i][j - 1]
+            if i < (row - 1):
+                perimeter -= grid[i + 1][j]
+            if j < (column - 1):
+                perimeter -= grid[i][j + 1]
     return perimeter
