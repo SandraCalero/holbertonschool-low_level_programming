22ec24cacad2aed95056832c8a6d224d109f9235
Sandra Calero <3358@holbertonschool.com>
2022-01-15 01:51:47 -0500

    add a function that searches for a value in a sorted array of integers using the Binary search algorithm

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,52 @@
+#include "search_algos.h"
+
+/**
+ * print_array - prints the array being searched every time it changes
+ * @array: Pointer to a pointer to the first element of the array to search in
+ * @left: First number at the left where the array starts printing
+ * @right: Last number at the left where the array finishes printing
+ */
+
+void print_array(int *array, int left, int right)
+{
+	int i;
+
+	for (i = left ; i < right ; i++)
+	{
+		printf("%d, ", array[i]);
+	}
+	printf("%d\n", array[i]);
+}
+
+/**
+ * binary_search - searches for a value in a sorted array of integers using the
+ * Binary search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @size: The number of elements in array
+ * @value: The value to search for
+ *
+ * Return: Index where value is located. -1 If value is not present in
+ * array or if array is NULL
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int left = 0;
+	int right = size - 1;
+	int i;
+
+	if (array != NULL)
+		while (left <= right)
+		{
+			printf("Searching in array: ");
+			print_array(array, left, right);
+			i = (left + right) / 2;
+			if (array[i] < value)
+				left = i + 1;
+			else if (array[i] > value)
+				right = i - 1;
+			else
+				return (i);
+		}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,6 +7,7 @@
 /*Mandatory tasks*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_array(int *array, int left, int right);
 
 /*Advanced tasks*/
 int jump_search(int *array, size_t size, int value);
